ea5e5a4dbb0b161ad9712318f4c8ec5e8d03b1e8
walid <541280314@qq.com>
2017-03-10 18:01:01 +0800

    提交插件自动构建

--- build/generator.js
+++ build/generator.js
@@ -11,6 +11,33 @@ const path = require('path')
 const chalk = require('chalk')
 const bundlePath = path.join('./build/output', 'bundle.json')
 
+// TODO 常用插件列表供开发者选择 后续会从网络上读取
+let plugins = [
+  {
+    "uri": "main",
+    "pkg": "com.osmartian.small.app.main"
+  },
+  {
+    "uri": "home",
+    "pkg": "com.osmartian.small.app.home"
+  },
+  {
+    "uri": "mine",
+    "pkg": "com.osmartian.small.app.mine"
+  },
+  {
+    "uri": "lib.style",
+    "pkg": "com.osmartian.small.lib.style"
+  },
+  {
+    "uri": "detail",
+    "pkg": "com.osmartian.small.app.detail",
+    "rules": {
+      "sub": "Sub"
+    }
+  }
+]
+
 let bundleJson = {
   version: '',
   bundles: []
@@ -38,7 +65,29 @@ function getName(name, message) {
   })
 }
 
-function addPlugin() {
+let index = 0
+
+function addDefaultPlugins() {
+  if (!plugins || plugins.length <= index) {
+    getName('Y', chalk.green(`Don't more defaul plugins，do your want add custom plugin ?(Y/n)`)).then(res => {
+      if (res.name.toLowerCase() === 'n') {
+        fs.writeFileSync(bundlePath, JSON.stringify(bundleJson))
+        return
+      }
+      addCustomPlugin()
+    })
+    return
+  }
+  getName('Y', chalk.green(`Are your sure add ${plugins[index].uri} plugin ?(Y/n)`)).then(res => {
+    if (res.name.toLowerCase() === 'y') {
+      bundleJson.bundles.push(plugins[index])
+    }
+    index++
+    addDefaultPlugins()
+  })
+}
+
+function addCustomPlugin() {
   let uri = 'main'
   let pkg = 'com.osmartian.small.app.main'
   getName('main', chalk.green('please input your plugin uri :')).then(res => {
@@ -52,7 +101,7 @@ function addPlugin() {
           fs.writeFileSync(bundlePath, JSON.stringify(bundleJson))
           return
         }
-        addPlugin()
+        addCustomPlugin()
       })
     })
   })
@@ -61,6 +110,6 @@ function addPlugin() {
 exports.generate = function () {
   getName('1.0.0', chalk.green('input your project version :')).then(res => {
     bundleJson.version = res.name
-    addPlugin()
+    addDefaultPlugins()
   })
 }
