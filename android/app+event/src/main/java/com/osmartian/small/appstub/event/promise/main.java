c82b3e52a008ec9dd470e8ab90a3e7fe78c3e507
walid <541280314@qq.com>
2017-04-08 18:02:36 +0800

    删除无用文件

--- android/app+event/src/main/java/com/osmartian/small/appstub/event/promise/main.java
+++ android/app+event/src/main/java/com/osmartian/small/appstub/event/promise/main.java
@@ -1,10 +1,22 @@
 package com.osmartian.small.appstub.event.promise;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class main {
 
     public static void main(String[] args) {
-        Promise<String, Integer> req = ExampleService.fakefetch(1000);
-        req.then(s -> {
+        Promise<String, Integer> promise = new Promise<>((resolve, reject) -> {
+            System.out.println("BEGIN");
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("END");
+                    resolve.run("walid =>");
+                }
+            }, 2000);
+        });
+        promise.then(s -> {
             System.out.println("then 1 : " + s);
             return s + s;
         }).then(s -> {
